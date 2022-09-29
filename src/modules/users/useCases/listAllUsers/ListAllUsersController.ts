a4182c3ead7c960973558940d6ecbd1f81f4ecd3
Gabriel Hernandes <gabrielmh13@hotmail.com>
2022-09-28 23:56:24 -0300

    Finalizado

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,10 +3,19 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = Array.isArray(request.headers) ? request.headers[0] : request.headers
+
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id })
+
+      return response.json(users)
+    }
+    catch (err) {
+      return response.status(400).json({ error: `${err}` })
+    }
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,19 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id)
+
+    if (user.admin === false || !user) {
+      throw new Error("Mensagem do erro")
+    }
+
+    const users = this.usersRepository.list()
+
+    return users
+
   }
 }
 
